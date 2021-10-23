168739efa75b11767932502e21ec399274f7dd67
Amit Kulkarni <amitkulkarni181@gmail.com>
2021-10-23 15:14:07 -0400

    Add create account page

--- my-react-app/App.js
+++ my-react-app/App.js
@@ -1,31 +1,77 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, SafeAreaView, Button, Alert} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>The Expo App is Working!</Text>
-      <Text>Here's another text</Text>
-      <StatusBar style="auto" />
+const SetupScreen = () => {
+	const [firstName, onChangeFirstName] = React.useState("");
+	const [lastName, onChangeLastName] = React.useState("");
+	const [email, onChangeEmail] = React.useState("");
+	const [phone, onChangePhone] = React.useState("");
 
+	return (
+		<SafeAreaView>
+			<Text style={styles.header}>
+					Enter Your Information
+			</Text>
+			<TextInput
+				style={styles.input}
+				onChangeText={onChangeFirstName}
+				value={firstName}
+				placeholder="First Name"
 
-      <TouchableOpacity
-        onPress={() => alert('Hello, world!')}
-        style={{ backgroundColor: 'blue' }}>
-        <Text style={{ fontSize: 20, color: '#fff' }}>Pick a photo</Text>
-      </TouchableOpacity>
+			/>
 
+			<TextInput
+				style={styles.input}
+				onChangeText={onChangeLastName}
+				value={lastName}
+				placeholder="Last Name"
+			/>
+			<TextInput
+				style={styles.input}
+				value={email}
+				onChangeText={onChangeEmail}
+				placeholder="Email"
+			/>
+			<TextInput
+				style={styles.input}
+				onChangeText={onChangePhone}
+				value={phone}
+				placeholder="Phone Number"
+				keyboardType="numeric"
+			/>
 
-    </View>
-  );
-}
+		<Button
+			title="Continue"
+			color="Red"
+			onPress={checkComplete}
+		></Button>
+		</SafeAreaView>
+	);
+
+	function checkComplete() {
+		console.log(firstName);
+		if(firstName.trim() == "" || lastName.trim() == "" || email.trim() == "" || phone.trim() == "") {
+			Alert.alert("Input is empty")
+		} 
+		
+	}	
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	header: {
+			fontSize: 25,
+			textAlign: 'center',
+			fontWeight: 'bold',
+			color: 'brown',
+	},
+	input: {
+		height: 40,
+		margin: 20,
+		borderWidth: 1,
+		padding: 10,
+	},
 });
+
+
+export default SetupScreen;
