73419bf45121352a68dc3d4a7b2b9cfe6afb0878
Amit Kulkarni <amitkulkarni181@gmail.com>
2021-10-23 09:51:25 -0400

    Adding stopwatch

--- my-react-app/App.js
+++ my-react-app/App.js
@@ -2,30 +2,75 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 
-export default function App() {
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+
+//     function timer(){
+//         var sec = 30;
+//         var timer = setInterval(function(){
+//             document.getElementById('safeTimerDisplay').innerHTML='00:'+sec;
+//             sec--;
+//             if (sec < 0) {
+//                 clearInterval(timer);
+//             }
+//         }, 1000);
+//     }
+
+
+//       {/* <Text>The Expo App is Working!</Text>
+//       <Text>Here's another text</Text>
+
+//       <Text>Time</Text>
+
+
+//       <StatusBar style="auto" />
+
+
+//       <TouchableOpacity
+//         onPress={() => alert('Welcome')}
+//         style={{ backgroundColor: 'blue' }}>
+//         <Text style={{ fontSize: 20, color: '#fff' }}>Login with Gmail</Text>
+//       </TouchableOpacity>
+
+//       <TouchableOpacity
+//         onPress={() => alert('Welcome!')}
+//         style={{ backgroundColor: 'yellow' }}>
+//         <Text style={{ fontSize: 20, color: '#fff' }}>Login with Facebook</Text>
+//       </TouchableOpacity>
+
+//       <TouchableOpacity
+//         onPress={() => alert('Welcome!')}
+//         style={{ backgroundColor: 'red' }}>
+//         <Text style={{ fontSize: 20, color: '#fff' }}>Login with Twitter</Text>
+//       </TouchableOpacity> */}
+
+//       <Text></Text>
+
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+
+
+import './App.css';
+import StopWatch from './Components/StopWatch/StopWatch.jsx';
+   
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>The Expo App is Working!</Text>
-      <Text>Here's another text</Text>
-      <StatusBar style="auto" />
-
-
-      <TouchableOpacity
-        onPress={() => alert('Hello, world!')}
-        style={{ backgroundColor: 'blue' }}>
-        <Text style={{ fontSize: 20, color: '#fff' }}>Pick a photo</Text>
-      </TouchableOpacity>
-
-
-    </View>
+    <div className="App">
+      <StopWatch />
+    </div>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  
+export default App;
